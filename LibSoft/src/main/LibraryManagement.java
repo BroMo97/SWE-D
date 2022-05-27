800aa648793e4eac40af5b287e48678ee612da5b
Muhammed Önal <muhammedoenal@outlook.com>
2022-05-27 23:12:49 +0200

    Full Update 2.0!

--- LibSoft/src/main/LibraryManagement.java
+++ LibSoft/src/main/LibraryManagement.java
@@ -1,5 +1,9 @@
 package main;
 
 public class LibraryManagement {
-	String university;
+	static String university = "FraUAS";
+	
+	public static void main(String[] args) {
+		System.out.println(university);
+	}
 }
